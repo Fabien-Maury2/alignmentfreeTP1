a86b3909341883161b7663b66bfef896a6503ea4
Fabien Maury <fabien.maury@etu.sorbonne-universite.fr>
2022-11-17 00:55:58 +0100

    Update kmers.py

--- kmers.py
+++ kmers.py
@@ -13,7 +13,7 @@ def kmer2str(val, k):
     str_val.reverse()
     return "".join(str_val)
 
-def nucl_compl(x):
+def nucl_compl(x): # returns the DNA nucleotide (a one-char string) complementary to the one given as input
   r = None
   if x == "A" or x == "a":
     r = "T"
@@ -25,7 +25,8 @@ def nucl_compl(x):
     r = "C"
   return(r)    
 
-def binarize(seq):
+def binarize(seq): # returns a binary string (made of "0" and "1) which is the binary translation of the input 
+  #DNA sequence (with the code C = 01, T = 10, G = 11, A and everything else = 00)
   hash = ""
   for i in range (len(seq)):
     if seq[i] == "G" or seq[i] == "g":
@@ -38,7 +39,7 @@ def binarize(seq):
       hash += "00"
   return(hash)
 
-def encode(seq):
+def encode(seq): # returns an integer number : the encoded form of the input binary string, (one sequence and its reverse complementary sequence should return the same number)
   if len(seq) > 1:
     hash1 = binarize(seq)
     hash1 = hash1[::-1]
@@ -66,7 +67,27 @@ def encode(seq):
       r = 0
   return(r)
 
-def stream_kmers(seq, k):
+def encode2(seq): # a more streamlined (and maybe faster ?) attempt to encode a binary string as a number (does not take reverse complementary into account)
+  total = 0
+  l = len(seq)
+  for i in range(l):
+    char = seq[l-(i+1)]
+    if char == "C" or char == "c":
+        a = 0
+        b = 1
+    elif char == "G" or char == "g":
+        a = 1
+        b = 1
+    elif char == "T" or char == "t":
+        a = 1
+        b = 0
+    else:
+        a = 0
+        b = 0
+    total += b*2**(2*i) + a*2**((2*i)+1)
+  return(total)
+
+def stream_kmers(seq, k): # returns a list of all kmers of length k contained in string seq
   list_kmer = []
   kmer = 0
   for i in range(k-1):
